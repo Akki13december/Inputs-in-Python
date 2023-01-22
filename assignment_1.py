0170278ddd6b2e4616e4aa1a16817eedc52c4bf6
Akki13december <116241702+Akki13december@users.noreply.github.com>
2023-01-21 18:09:26 -0800

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,8 @@
+"""
+Assignment 1: this is a basic input and output file
+January 19, 2023
+Akanksha Khare
+"""
+name = input("What is your name?\n")
+input("Do you know Python?\n")
+print("Thanks for answering my questions, " + name + ".")
